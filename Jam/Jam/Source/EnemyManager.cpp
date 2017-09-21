c35f2d88af9dfd4738e96ba12a721328598cd1d3
Ow Hong Yu <ohy99@hotmail.com>
2017-09-21 18:38:13 +0800

    unspawn on dead

--- Jam/Jam/Source/EnemyManager.cpp
+++ Jam/Jam/Source/EnemyManager.cpp
@@ -6,7 +6,11 @@ void EnemyManager::update(double dt)
 {
 	for each (auto &e in enemy_pool)
 		if (e->active)
+		{
 			e->update(dt);
+			if (e->hitpoint.get_hp_percentage() <= 0)
+				e->deactivate();
+		}
 }
 
 Enemy * EnemyManager::get_inactive()
